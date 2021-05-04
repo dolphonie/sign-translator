1f2123228bbe1d387cbef1dd6c55079e37602b2d
Patrick Kao <patrick.d.kao@gmail.com>
2021-05-04 16:05:04 -0400

    Added main guard

--- train.py
+++ train.py
@@ -6,8 +6,9 @@ from config import Config
 from data.lrs3 import LRS3DataModule
 from model.sign_translator import SignTranslator
 
-data = LRS3DataModule(Config)
-model = SignTranslator(Config)
+if __name__ == '__main__':
+    data = LRS3DataModule(Config)
+    model = SignTranslator(Config)
 
-trainer = Trainer(**Config.trainer_params)
-trainer.fit(model, data)
+    trainer = Trainer(**Config.trainer_params)
+    trainer.fit(model, data)
