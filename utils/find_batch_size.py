337bcd917a74cd8b4ccfaca9c9a1299ba732602b
Patrick Kao <patrick.d.kao@gmail.com>
2021-05-04 17:04:28 -0400

    Added find batch size

---
+++ utils/find_batch_size.py
@@ -0,0 +1,19 @@
+# Created by Patrick Kao
+from pytorch_lightning import Trainer
+from pytorch_lightning.tuner.tuning import Tuner
+from torch import nn
+
+from config import Config
+from data.lrs3 import LRS3DataModule
+from model.sign_translator import SignTranslator
+
+if __name__ == "__main__":
+    data = LRS3DataModule(Config)
+    model = SignTranslator(Config)
+    trainer = Trainer()
+    tuner = Tuner(trainer)
+
+    # Invoke method
+    new_batch_size = tuner.scale_batch_size(model)
+
+    print(f"Biggest batch that fits: {new_batch_size}")
