0ca66d7ed29bc80a3d5abbaa6220d8cd611b9984
Patrick Kao <patrick.d.kao@gmail.com>
2021-05-09 13:47:52 -0400

    remove slurm vars

--- train.py
+++ train.py
@@ -1,4 +1,6 @@
 # Created by Patrick Kao
+import os
+
 from pytorch_lightning import Trainer
 from torch import nn
 
@@ -7,6 +9,9 @@ from data.lrs3 import LRS3DataModule
 from model.sign_translator import SignTranslator
 
 if __name__ == '__main__':
+    del os.environ["SLURM_NTASKS"]
+    del os.environ["SLURM_JOB_NAME"]
+
     data = LRS3DataModule(Config)
     model = SignTranslator(Config)
 
