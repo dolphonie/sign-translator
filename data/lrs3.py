600cf4b87f3de2a600faefc2ab3c89072178bb3c
Patrick Kao <patrick.d.kao@gmail.com>
2021-05-11 15:56:53 -0400

    remoev shuffle

--- data/lrs3.py
+++ data/lrs3.py
@@ -109,12 +109,10 @@ class LRSDataModule(LightningDataModule):
 
     def train_dataloader(self) -> Any:
         return DataLoader(self.train_dataset, batch_size=self.batch_size,
-                          collate_fn=collate_batch, shuffle=True)
+                          collate_fn=collate_batch)
 
     def val_dataloader(self) -> Union[DataLoader, List[DataLoader]]:
-        return DataLoader(self.val_dataset, batch_size=self.batch_size, collate_fn=collate_batch,
-                          shuffle=False)
+        return DataLoader(self.val_dataset, batch_size=self.batch_size, collate_fn=collate_batch)
 
     def test_dataloader(self) -> Union[DataLoader, List[DataLoader]]:
-        return DataLoader(self.test_dataset, batch_size=self.batch_size, collate_fn=collate_batch,
-                          shuffle=False)
+        return DataLoader(self.test_dataset, batch_size=self.batch_size, collate_fn=collate_batch)
