5d22a964a3870a2099f8beace4b3fb1bcb8520f2
Patrick Kao <patrick.d.kao@gmail.com>
2021-05-11 17:28:30 -0400

    fixed find batch

--- utils/find_batch_size.py
+++ utils/find_batch_size.py
@@ -1,16 +1,36 @@
 # Created by Patrick Kao
-from pytorch_lightning import Trainer
+import argparse
+import os
+
 from pytorch_lightning.tuner.tuning import Tuner
-from torch import nn
 
-from config import Config
-from data.lrs3 import LRSDataModule
-from model.sign_translator import SignTranslator
 
-if __name__ == "__main__":
-    data = LRSDataModule(Config)
-    model = SignTranslator(Config)
-    trainer = Trainer()
+def remove_slurm_vars():
+    for k, v in os.environ.items():
+        if "SLURM" in k:
+            print(f"Deleting env variable {k}")
+            del os.environ[k]
+
+
+if __name__ == '__main__':
+    remove_slurm_vars()
+
+    from pytorch_lightning import Trainer
+
+    from config import Config, LRS2Config
+    from data.lrs3 import LRSDataModule
+    from model.sign_translator import SignTranslator
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--lrs2", action="store_true", help="Use the LRS2 Dataset params")
+    args = parser.parse_args()
+
+    config = Config
+    if args.lrs2:
+        config = LRS2Config
+    data = LRSDataModule(config)
+    model = SignTranslator(config)
+    trainer = Trainer(**Config.trainer_params)
     tuner = Tuner(trainer)
 
     # Invoke method
