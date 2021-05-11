dafcd804b7d8d7d2b95ca03e88c71a93dd14a80d
Patrick Kao <patrick.d.kao@gmail.com>
2021-05-10 23:23:12 -0400

    remove mnist

--- example.py
+++ example.py
@@ -10,47 +10,48 @@ from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.datasets import MNIST
 
+if __name__ == "__main__":
 
-class LitMNIST(LightningModule):
+    class LitMNIST(LightningModule):
 
-    def __init__(self):
-        super().__init__()
+        def __init__(self):
+            super().__init__()
 
-        # mnist images are (1, 28, 28) (channels, width, height)
-        self.layer_1 = nn.Linear(28 * 28, 128)
-        self.layer_2 = nn.Linear(128, 256)
-        self.layer_3 = nn.Linear(256, 10)
+            # mnist images are (1, 28, 28) (channels, width, height)
+            self.layer_1 = nn.Linear(28 * 28, 128)
+            self.layer_2 = nn.Linear(128, 256)
+            self.layer_3 = nn.Linear(256, 10)
 
-    def forward(self, x):
-        batch_size, channels, width, height = x.size()
+        def forward(self, x):
+            batch_size, channels, width, height = x.size()
 
-        # (b, 1, 28, 28) -> (b, 1*28*28)
-        x = x.view(batch_size, -1)
-        x = self.layer_1(x)
-        x = F.relu(x)
-        x = self.layer_2(x)
-        x = F.relu(x)
-        x = self.layer_3(x)
+            # (b, 1, 28, 28) -> (b, 1*28*28)
+            x = x.view(batch_size, -1)
+            x = self.layer_1(x)
+            x = F.relu(x)
+            x = self.layer_2(x)
+            x = F.relu(x)
+            x = self.layer_3(x)
 
-        x = F.log_softmax(x, dim=1)
-        return x
+            x = F.log_softmax(x, dim=1)
+            return x
 
-    def training_step(self, batch, batch_idx):
-        x, y = batch
-        logits = self(x)
-        loss = F.nll_loss(logits, y)
-        return loss
+        def training_step(self, batch, batch_idx):
+            x, y = batch
+            logits = self(x)
+            loss = F.nll_loss(logits, y)
+            return loss
 
-    def configure_optimizers(self):
-        return Adam(self.parameters(), lr=1e-3)
+        def configure_optimizers(self):
+            return Adam(self.parameters(), lr=1e-3)
 
-transform=transforms.Compose([transforms.ToTensor(),
-                              transforms.Normalize((0.1307,), (0.3081,))])
+    transform=transforms.Compose([transforms.ToTensor(),
+                                  transforms.Normalize((0.1307,), (0.3081,))])
 
-# data
-mnist_train = MNIST(os.getcwd(), train=True, download=False, transform=transform)
-mnist_train = DataLoader(mnist_train, batch_size=64)
+    # data
+    mnist_train = MNIST(os.getcwd(), train=True, download=False, transform=transform)
+    mnist_train = DataLoader(mnist_train, batch_size=64)
 
-model = LitMNIST()
-trainer = Trainer(gpus=-1 if torch.cuda.is_available() else None)
-trainer.fit(model, mnist_train)
+    model = LitMNIST()
+    trainer = Trainer(gpus=-1 if torch.cuda.is_available() else None)
+    trainer.fit(model, mnist_train)
